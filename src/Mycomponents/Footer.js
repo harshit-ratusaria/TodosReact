654e1c80a351b9af513b5000de8929a4ac2bf576
Harshit <hratusaria@gmail.com>
2025-01-11 11:39:53 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/Mycomponents/Footer.js

--- src/Mycomponents/Footer.js
+++ src/Mycomponents/Footer.js
@@ -2,7 +2,8 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <div className='bg-dark text-light'>Copyright &copy; MyTodosList.com
+    <div className='bg-dark text-light py-3'>
+      <p className="text-center">Copyright &copy; MyTodosList.com</p>
     </div>
   )
 }
